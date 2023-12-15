e9b78d13cc774879f968b6b7a5fc23d5993561f8
Morgenthaler, Sven <Sven.Morgenthaler@vector.com>
2023-12-15 14:06:45 +0100

    SKA-274: Changed folder name from vTestStudio to vTESTstudio (#79)
    - folder and file renaming
    - recompiled test unit
    - replaced Test Units in CANoe configs

--- chardev/demos/CANoe4SW_SE/run.ps1
+++ chardev/demos/CANoe4SW_SE/run.ps1
@@ -9,7 +9,7 @@ $canoe4sw_se_install_dir = $env:CANoe4SWSE_InstallDir64
 & $canoe4sw_se_install_dir/environment-make.exe "$PSScriptRoot/venvironment.yaml"  -o "$PSScriptRoot"
 
 #compile test unit
-& $canoe4sw_se_install_dir/test-unit-make.exe "$PSScriptRoot/../vTestStudio/TestUnit" -e "$PSScriptRoot/Default.venvironment" -o "$PSScriptRoot"
+& $canoe4sw_se_install_dir/test-unit-make.exe "$PSScriptRoot/../vTESTstudio/TestUnit" -e "$PSScriptRoot/Default.venvironment" -o "$PSScriptRoot"
 
 #run tests
 & $canoe4sw_se_install_dir/canoe4sw-se.exe "$PSScriptRoot/Default.venvironment" -d "$PSScriptRoot/working-dir" --verbosity-level "2" --test-unit "$PSScriptRoot/TestUnit.vtestunit"  --show-progress "tree-element"
--- eth/demos/CANoe4SW_SE/run.sh
+++ eth/demos/CANoe4SW_SE/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PSScriptRoot=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-canoe4sw_se_install_dir="/home/vector/CANoe4SW_SE_16_4_4"
+canoe4sw_se_install_dir="/home/vector/canoe4sw-se_17.3.91"
 
 #run tests
 $canoe4sw_se_install_dir/canoe4sw-se "$PSScriptRoot/Default.venvironment" -d "$PSScriptRoot/working-dir" --verbosity-level "2" --test-unit "$PSScriptRoot/TestUnit.vtestunit"  --show-progress "tree-element"
