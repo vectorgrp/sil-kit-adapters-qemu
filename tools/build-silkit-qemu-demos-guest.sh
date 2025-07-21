c1cf80a354966df3dc4f593d8ab0dbc881538f27
Aubry, Kyllian <Kyllian.Aubry@vector.com>
2025-07-21 08:58:31 +0200

    SKA-787: update build qemu image script (#112)

--- tools/build-silkit-qemu-demos-guest.sh
+++ tools/build-silkit-qemu-demos-guest.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 virt-builder debian-11                                             \
-    --output silkit-qemu-demos-guest.qcow2                            \
+    --no-network                                                   \
+    --output silkit-qemu-demos-guest.qcow2                         \
     --format qcow2                                                 \
-    --hostname silkit-qemu-demos-guest                                \
+    --hostname silkit-qemu-demos-guest                             \
     --root-password password:root                                  \
     --run ./tools/virt-builder/debian/install.100.setup-network.sh \
     --run ./tools/virt-builder/debian/install.110.setup-sshd.sh
