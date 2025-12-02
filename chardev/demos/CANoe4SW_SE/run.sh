738fab81fc1d1304a58a34a37192f7c6adf163d2
Aubry, Kyllian <Kyllian.Aubry@vector.com>
2025-12-02 11:45:46 +0100

    SKA-923: split trigger workflow into two (#150)

--- chardev/demos/CANoe4SW_SE/run.sh
+++ chardev/demos/CANoe4SW_SE/run.sh
@@ -3,7 +3,7 @@
 
 #!/bin/bash
 PSScriptRoot=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-canoe4sw_se_install_dir="/opt/vector/canoe4sw-se"
+canoe4sw_se_install_dir="/opt/vector/canoe-server-edition"
 
 #run tests
 $canoe4sw_se_install_dir/canoe4sw-se "$PSScriptRoot/Default.venvironment" -d "$PSScriptRoot/working-dir" --verbosity-level "2" --test-unit "$PSScriptRoot/testQemuEchoDemo.vtestunit"  --show-progress "tree-element"
--- eth/demos/CANoe4SW_SE/run.sh
+++ eth/demos/CANoe4SW_SE/run.sh
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: MIT
 
 PSScriptRoot=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-canoe4sw_se_install_dir="/opt/vector/canoe4sw-se"
+canoe4sw_se_install_dir="/opt/vector/canoe-server-edition"
 
 #run tests
 $canoe4sw_se_install_dir/canoe4sw-se "$PSScriptRoot/Default.venvironment" -d "$PSScriptRoot/working-dir" --verbosity-level "2" --test-unit "$PSScriptRoot/testQemuPingDemo.vtestunit"  --show-progress "tree-element"
